ec85cf36b7745a0e6cd66ab64e9812fa5583613f
stuart-hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-08-23 19:52:05 -0400

    feat: implement basic calculator functions
    - Added `add` function to perform addition of two numbers.
    - Added `subtract` function to perform subtraction of two numbers.
    - Added `sum` function to calculate the sum of an array of numbers.
    - Added `multiply` function to calculate the product of an array of numbers.
    - Added `power` function to raise a number to the power of another number using exponentiation operator.
    - Added `factorial` function to compute the factorial of a number with handling for negative inputs.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, num) => (total *= num), 1);
 };
 
-const power = function() {
-	
+const power = function (num1, power) {
+  return num1 ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num < 0) {
+    return "ERROR";
+  } else if (num === 0) {
+    return 1;
+  }
+  let total = 1;
+  do {
+    total *= num;
+    num--;
+  } while (num > 0);
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
