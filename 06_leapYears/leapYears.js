5ca07fb9e7ae222424c744cbc9b5e979ef9e9ee6
stuart-hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-08-23 19:09:44 -0400

    feat: add leap year function with input validation
    - Implemented the `leapYears` function to determine if a given year is a leap year.
    - The function checks if the input is a number and returns `"ERROR"` for non-number inputs.
    - Leap year logic includes conditions for divisibility by 400, 100, and 4.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (typeof year !== "number") {
+    return "ERROR";
+  }
 
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  }
+
+  return year % 4 === 0;
 };
 
 // Do not edit below this line
