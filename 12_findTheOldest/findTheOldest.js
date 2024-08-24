cc3db78a07aaec869878a5d1f9938a0298b129ec
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-08-24 14:42:36 -0400

    feat: solve findTheOldest using a `for of` loop
    - stores two variables: oldestAge and oldestPerson
    - checks each person's age vs the oldestPerson
    - updates oldestAge and oldestPerson when necessary
    - uses new Date().getFullYear() to calculate current year

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0
+    let oldestPerson = {}
 
+    for (let person of people) {
+        let age
+        if (!person.yearOfDeath) {
+            year = new Date().getFullYear()
+            age = year - person.yearOfBirth
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth
+        }
+        if (age > oldestAge) {
+            oldestAge = age
+            oldestPerson = person
+        }
+    }
+
+    return oldestPerson
 };
 
 // Do not edit below this line
