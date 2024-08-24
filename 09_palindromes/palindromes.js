29445c21f9fca9b3153396e050548b1afc4ccae6
stuart-hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-08-23 20:57:57 -0400

    feat: build solution to palindromes question
    - normalize string
        - lowercase letters
        - remove non-alphanumeric characters
    - reverse string
        - split, reverse, join
    - compare clean string and reversed string for equality

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const lowerCaseString = str.toLowerCase();
 
+  let cleanedStr = "";
+
+  for (let char of lowerCaseString) {
+    if ((char >= "a" && char <= "z") || (char >= "0" && char <= "9")) {
+      cleanedStr += char;
+    }
+  }
+
+  const reversedStr = cleanedStr.split("").reverse().join("");
+  return reversedStr === cleanedStr;
 };
 
 // Do not edit below this line
