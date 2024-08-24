08ed4524db7ddffb0cd3d79e85dae2c48cd5bcc0
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-08-24 14:00:08 -0400

    feat: solve fibonacci using O(n) space complexity

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(end) {
+    let fib = [1, 1]
+    if (end < 0) {
+        return "OOPS"
+    }
+    if (end == 0) {
+        return 0
+    }
+    if (end == 1 || end == 2) {
+        return 1
+    }
+    for (let i = 2; i < end; i++) {
+        fib.push(fib[fib.length - 1] + fib[fib.length - 2])
+    }
+    return fib[fib.length - 1]
 };
 
 // Do not edit below this line
